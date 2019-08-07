07d7030d79dfe0fea4259d224b56672ae15f3ced
Guy Arieli <guyarieli1997@gmail.com>
2019-08-07 21:47:42 +0300

    add home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/Layout';
+import Profile from '../components/Profile';
+import '../styles/base.scss';
 
-export default () => <div>Hello world!</div>
+const indexPage = () => {
+  return (
+    <div id="index-page">
+      <Layout>
+        <Profile />
+      </Layout>
+    </div>
+  );
+};
+
+export default indexPage;
